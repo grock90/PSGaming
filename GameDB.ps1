89adbba8c173373b904e38c19188f50be1b93aa6
Gary Barnes <gbarnes@webmd.net>
2019-04-19 11:23:19 -0700

    initial commit

---
+++ GameDB.ps1
@@ -0,0 +1,27 @@
+﻿Param
+(
+    [Parameter(Mandatory)]
+    $DatabaseName,
+
+    [ValidateScript({Test-Path $_})]
+    [Parameter(Mandatory)]
+    $DBPath
+)
+
+Install-Module -Name PSSQLite
+
+$Database = ('{0}\{1}.SQLite' -f $DBPath,$DatabaseName)
+
+if(!(Test-Path -Path $Database))
+{
+    $Query = "CREATE TABLE NAMES (
+        UserName VARCHAR(20) PRIMARY KEY,
+        Age INT,
+        Location TEXT,
+        Rank INT)"
+    Invoke-SqliteQuery -Query $Query -DataSource $Database
+
+    Invoke-SqliteQuery -DataSource $Database -Query "PRAGMA table_info(NAMES)"
+}
+
+
---
+++ dice.ps1
@@ -0,0 +1,27 @@
+﻿class Dice
+{
+    [int]$MaxValue
+    [int]$CurrentValue
+    [int]$LastValue
+
+    Dice([int]$MaxValue){
+        $this.MaxValue = $MaxValue
+    }
+
+    [void] Roll()
+    {
+        $this.LastValue = $this.CurrentValue
+        $this.CurrentValue = Get-Random -Minimum 1 -Maximum $this.MaxValue
+    }
+
+    [void] Show()
+    {
+        [System.Windows.MessageBox]::Show($this.CurrentValue,'Dice Roll','OK')
+    }
+
+    [void] Reset()
+    {
+        $this.CurrentValue = 0
+        $this.LastValue = 0
+    }
+}
